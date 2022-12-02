c731cc64a53ce418b9df9d0b53b87741f030153f
eeMji89 <110209389+eeMji89@users.noreply.github.com>
2022-12-02 14:33:17 -0600

    gfd

---
+++ src/Lab9p2/Hilocombate.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab9p2;
+
+/**
+ *
+ * @author ELIZABETH HERNANDEZ
+ */
+public class Hilocombate extends Thread{
+    
+    
+}
